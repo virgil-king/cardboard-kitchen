ac4e6b7b6984485d128e56f50509bedf20797634
Virgil King <virgilking@gmail.com>
2024-05-01 10:59:14 -0700

    Move all tsbuildinfo files into out/

--- packages/util/util.test.ts
+++ packages/util/util.test.ts
@@ -1,13 +1,6 @@
 import { assert, test } from "vitest";
 import { combineHashes } from "./util.js";
 
-// test("shuffle result has same length", () => {
-//   assert(
-//     shuffle([1, 5, 9]).length == 3,
-//     "shuffled length should be unchanged"
-//   );
-// });
-
 test("combineHashes: two inputs: result does not equal either input", () => {
   const result = combineHashes([3, 9]);
 
