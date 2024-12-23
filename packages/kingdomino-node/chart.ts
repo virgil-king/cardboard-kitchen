d88ea292a7eaee4531c4dfc6172be08344fe5abd
Virgil King <virgilking@gmail.com>
2024-12-22 18:45:21 -0800

    WIP log.json charting

---
+++ packages/kingdomino-node/chart.ts
@@ -0,0 +1,53 @@
+import { readFile } from "node:fs/promises";
+import * as Plot from "@observablehq/plot";
+import { JSDOM } from "jsdom";
+import { kingdominoConv7 } from "./config.js";
+import { EvalLogEntry } from "./eval-worker.js";
+import { requireDefined } from "studio-util";
+
+const log: Array<EvalLogEntry> = JSON.parse(
+  await readFile(await kingdominoConv7.logFile(), {
+    encoding: "utf-8",
+  })
+);
+
+function computeChartData() {
+  return log.flatMap((logEntry) => {
+    const frames = requireDefined(logEntry.modelMetadata).trainingSampleCount;
+    return logEntry.results.map(([playerId, result]) => {
+      return {
+        playerId: playerId,
+        frames: frames,
+        value: result.value,
+      };
+    });
+  });
+}
+
+const chartData = computeChartData();
+
+const plot = Plot.plot({
+  document: new JSDOM("").window.document,
+  // The following line currently causes the whole chart to become invisible
+  // color: { legend: true },
+  marks: [
+    Plot.line(chartData, {
+      x: "frames",
+      y: "value",
+      stroke: "playerId",
+    }),
+  ],
+});
+
+plot.setAttributeNS(
+  "http://www.w3.org/2000/xmlns/",
+  "xmlns",
+  "http://www.w3.org/2000/svg"
+);
+plot.setAttributeNS(
+  "http://www.w3.org/2000/xmlns/",
+  "xmlns:xlink",
+  "http://www.w3.org/1999/xlink"
+);
+
+process.stdout.write(plot.outerHTML);
